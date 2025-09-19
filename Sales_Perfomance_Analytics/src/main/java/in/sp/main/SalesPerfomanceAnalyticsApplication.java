8f940405ef61999ec0159e1632bf614a0cdf9f22
KUYYA ANURAG <69108812+anurag01502@users.noreply.github.com>
2025-09-19 19:12:14 +0530

    Update SalesPerfomanceAnalyticsApplication.java

--- Sales_Perfomance_Analytics/src/main/java/in/sp/main/SalesPerfomanceAnalyticsApplication.java
+++ Sales_Perfomance_Analytics/src/main/java/in/sp/main/SalesPerfomanceAnalyticsApplication.java
@@ -3,6 +3,8 @@ package in.sp.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Main Application
+
 @SpringBootApplication
 public class SalesPerfomanceAnalyticsApplication {
 
